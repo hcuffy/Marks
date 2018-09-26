5429a968e69b82a2502ec98894604dc4fcee3555
Henry Cuffy <hcuffy_5@hotmail.com>
2018-09-26 17:24:47 +0200

    fix school form

--- app/components/School.js
+++ app/components/School.js
@@ -4,18 +4,18 @@ import { bindActionCreators} from 'redux';
 import { actionCreators } from '../actions/index';
 import SideMenu from './SideMenu';
 
-const School = () => (
+const School = ({actions}) => (
 
     <div className="form-wrapper">
       <SideMenu/>
-        <form method="POST" >
-        <h2>School</h2>
+        <form onSubmit={actions.handleSchoolData} method="POST" method="POST" >
+        <h2>School Information</h2>
           <input name="title" type="text" placeholder="Enter your school's name."/>
           <input name="street" type="text" placeholder="Enter the street."/>
           <input name="state" type="text" placeholder="Enter the state."/>
           <input name="country" type="text" placeholder="Enter the country."/>
           <input name="year" type="number" min="2000" max="2050" placeholder="Enter the country."/>
-          <button className="custom-btn">Login</button>
+          <button className="custom-btn">Save</button>
         </form>
     </div>
   );
