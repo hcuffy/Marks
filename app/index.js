06fa542dc2173fd6fc07b88ecd745d5b4aad3971
Henry Cuffy <hcuffy_5@hotmail.com>
2018-10-08 16:14:06 +0200

    Fix loading issue

--- app/index.js
+++ app/index.js
@@ -7,6 +7,13 @@ import './app.global.css';
 
 const store = configureStore();
 
+render(
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+
 if (module.hot) {
   module.hot.accept('./containers/Root', () => {
     const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
@@ -17,12 +24,4 @@ if (module.hot) {
       document.getElementById('root')
     );
   });
-}else{
-
-  render(
-    <AppContainer>
-      <Root store={store} history={history} />
-    </AppContainer>,
-    document.getElementById('root')
-  );
 }
