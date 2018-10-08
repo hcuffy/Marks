71bcc9b3f32c83219dd4471ecdd229efe3337c0f
Henry Cuffy <hcuffy_5@hotmail.com>
2018-10-08 07:20:58 +0200

    Adjust render

--- app/index.js
+++ app/index.js
@@ -7,13 +7,6 @@ import './app.global.css';
 
 const store = configureStore();
 
-render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById('root')
-);
-
 if (module.hot) {
   module.hot.accept('./containers/Root', () => {
     const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
@@ -24,4 +17,12 @@ if (module.hot) {
       document.getElementById('root')
     );
   });
+}else{
+
+  render(
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('root')
+  );
 }
