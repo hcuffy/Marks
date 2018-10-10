b5d5cae0485bcd9fcde9313563d9ebbdbb5f7675
Henry Cuffy <hcuffy_5@hotmail.com>
2018-10-10 16:36:25 +0200

    fix route

--- app/Routes.js
+++ app/Routes.js
@@ -4,12 +4,12 @@ import { Switch, Route } from 'react-router';
 import routes from './constants/routes.json';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import SchoolPage from './containers/SchoolPage';
+import School from './containers/SchoolPage';
 
 export default () => (
   <App>
     <Switch>
-      <Route path={routes.SCHOOL} component={SchoolPage} />
+      <Route path={routes.SCHOOL} component={School} />
       <Route path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './styles/Home.css';
 import SideMenu from './SideMenu';
-import {generalSuccess} from './notifications/General';
 import { toast } from 'react-toastify';
 
 type Props = {};
@@ -16,7 +15,6 @@ export default class Home extends Component<Props> {
     return (
       <div className={styles.container} data-tid="container">
         <SideMenu/>
-        <button onClick={generalSuccess()}>test</button>
 
       </div>
     );
