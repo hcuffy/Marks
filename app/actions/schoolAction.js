964f4e3034b55619281132b0a01b1c7d21336de2
Henry Cuffy <hcuffy_5@hotmail.com>
2018-10-15 18:04:22 +0200

    add zip and year

--- app/actions/schoolAction.js
+++ app/actions/schoolAction.js
@@ -14,7 +14,9 @@ export const handleSchoolData = (event) => {
     title: event.target.title.value,
     street: event.target.street.value,
     state: event.target.state.value,
-    country: event.target.country.value
+    country: event.target.country.value,
+    zip: event.target.zip.value,
+    year: event.target.year.value
   }
 
   addSchoolData(formData)
