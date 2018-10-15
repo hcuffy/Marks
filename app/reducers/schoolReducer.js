e4020e9a1b78b6a2cb00e91b9c25d51080482a23
Henry Cuffy <hcuffy_5@hotmail.com>
2018-10-15 18:03:58 +0200

    add zip and year

--- app/reducers/schoolReducer.js
+++ app/reducers/schoolReducer.js
@@ -2,18 +2,12 @@
 import { HANDLE_SCHOOL_DATA } from '../actions/actionTypes';
 import type { Action } from './types';
 
-const initialLoadState = {
-  'title':'placeholder',
-  'street':'placeholder',
-  'state':'placeholder',
-  'country':'placeholder',
-  'year':'placeholder'
-}
+const initialLoadState = {}
 
 const handleSchoolReducer = (state = initialLoadState, action) => {
   switch (action.type) {
     case HANDLE_SCHOOL_DATA:
-       const { title, street, state, country, year } = action.payload;
+       const { title, street, state, country, zip, year } = action.payload;
        return Object.assign({}, state, { title, street, state, country, year });
     default:
       return Object.assign({},initialLoadState);
