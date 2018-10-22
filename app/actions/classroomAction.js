947595b2fa149dfd66b6b40cc96bd1c9e4b9d001
Henry Cuffy <hcuffy_5@hotmail.com>
2018-10-22 17:03:28 +0200

    Add action

---
+++ app/actions/classroomAction.js
@@ -0,0 +1,7 @@
+// @flow
+import { CHANGE_CLASSROOM_TAB } from './actionTypes'
+
+export const changeClassroomTab = () => ({
+  type: CHANGE_CLASSROOM_TAB,
+  payload: { schoolData: formData }
+})
--- app/actions/index.js
+++ app/actions/index.js
@@ -1,6 +1,8 @@
 import { handleSchoolData, displaySchoolData } from './schoolAction'
+import { changeClassroomTab } from './classroomAction'
 
 export const actionCreators = {
   handleSchoolData,
-  displaySchoolData
+  displaySchoolData,
+  changeClassroomTab
 }
