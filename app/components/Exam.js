a26f5f14c42d05574fcf3412b2d83d67000097f2
Henry Cuffy <hcuffy_5@hotmail.com>
2018-10-27 08:18:55 +0200

    Add exam component

---
+++ app/components/Exam.js
@@ -0,0 +1,23 @@
+// @flow
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from '../actions/index'
+import styles from './styles/classroom.css'
+
+const Exam = () => (
+  <div className={styles.room_div}>
+    <p>In Exams</p>
+  </div>
+)
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(actionCreators, dispatch)
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Exam)
--- app/containers/Classroom.js
+++ app/containers/Classroom.js
@@ -6,24 +6,33 @@ import { actionCreators } from '../actions/index'
 import SideMenu from '../components/SideMenu'
 import NavBar from '../components/NavBar'
 import Classes from '../components/Classes'
+import Exam from '../components/Exam'
 
 class Classroom extends Component {
   render() {
+    console.log(this.props.classesActive)
+    console.log(this.props.examActive)
     return (
       <div data-tid="classroom_container">
         <SideMenu />
         <NavBar />
-        <Classes />
+        {this.props.classesActive && <Classes />}
+        {this.props.examActive && <Exam />}
       </div>
     )
   }
 }
+
+const mapStateToProps = state => ({
+  classesActive: state.tabStatus.subjectTab,
+  examActive: state.tabStatus.testTab
+})
 
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators(actionCreators, dispatch)
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Classroom)
