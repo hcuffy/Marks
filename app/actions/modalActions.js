42c26b690ae10b32edb91231f35b6c757c9641a0
Henry Cuffy <hcuffy_5@hotmail.com>
2018-11-02 18:22:54 +0100

    Add room modal action

---
+++ app/actions/modalActions.js
@@ -0,0 +1,19 @@
+import { toast } from 'react-toastify'
+import { DISPLAY_ROOM_MODAL } from './actionTypes'
+
+export const handleClassData = event => async dispatch => {
+  event.preventDefault()
+
+  const classData = {
+    Name: event.target.Name.value,
+    Teacher: event.target.Teacher.value,
+    Code: event.target.Code.value,
+    Subject_Teacher: event.target.Subject_Teacher.value
+  }
+  event.target.reset()
+
+  dispatch({
+    type: DISPLAY_ROOM_MODAL,
+    payload: { classData }
+  })
+}
