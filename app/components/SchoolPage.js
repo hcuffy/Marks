409cc31082aa8171d8dd97e424b06c8a805f3ca3
Henry Cuffy <hcuffy_5@hotmail.com>
2018-11-14 19:04:38 +0100

    add homepage place holders

--- app/components/SchoolPage.js
+++ app/components/SchoolPage.js
@@ -9,9 +9,28 @@ const _ = require('lodash')
 const SchoolPage = ({ schoolData }) => {
 	const entry = _.values(schoolData).map((data, idx) => <li key={idx}>{data}</li>)
 	return (
-		<div className={styles.school_data_div}>
-			<p>Display Test</p>
-			<span>{entry}</span>
+		<div className={styles.main_school_div}>
+			<div className={styles.school_left_div}>
+				<div>
+					<p>Your School Address</p>
+					<span>{entry}</span>
+				</div>
+				<div className={styles.page_inner_div}>
+					<p>Your Classrooms</p>
+					<span>Under Development</span>
+				</div>
+			</div>
+
+			<div className={styles.school_right_div}>
+				<div>
+					<p>Your Students</p>
+					<span>Under Development</span>
+				</div>
+				<div className={styles.page_inner_div}>
+					<p>Your Subjects</p>
+					<span>Under Development</span>
+				</div>
+			</div>
 		</div>
 	)
 }
