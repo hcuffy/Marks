6c8a8dfa29fd970313af90b7f6a85d23c01e1a67
Henry Cuffy <hcuffy_5@hotmail.com>
2018-11-23 19:13:00 +0100

    Add subject component and display it

---
+++ app/components/SubjectList.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from '../actions/index'
+import styles from './styles/list.css'
+
+const _ = require('lodash')
+
+function filterSubjects(subjectData, chosenClass) {
+	if (_.isNil(subjectData) || _.isNil(chosenClass) || chosenClass === 'Select Class') {
+		return []
+	}
+
+	const chosenSubjects = _.chain(subjectData)
+		.filter(['Room', chosenClass.Name])
+		.orderBy(['Abbreviation'], [subJ => subJ.Abbreviation.toLowerCase()], ['asc'])
+		.value()
+	return chosenSubjects
+}
+
+const SubjectList = ({ selectedSubject, subjectData }) => {
+	const filteredData = filterSubjects(subjectData.data, selectedSubject)
+
+	const listInputs = filteredData.map((data, idx) => (
+		<button
+			key={idx}
+			id={data._id}
+			type="button"
+			className={`list-group-item list-group-item-action ${styles.list_btn}`}
+		>
+			{data.Abbreviation}
+			<span className={`badge badge-warning badge-pill ${styles.badge_number}`}>
+				{data.Tests.length}
+			</span>
+		</button>
+	))
+
+	return (
+		<div className={styles.list_div}>
+			<div className="list-group list-group-flush">{listInputs}</div>
+		</div>
+	)
+}
+const mapStateToProps = state => ({
+	subjectData: state.subjectData
+})
+
+const mapDispatchToProps = dispatch => ({
+	actions: bindActionCreators(actionCreators, dispatch)
+})
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(SubjectList)
