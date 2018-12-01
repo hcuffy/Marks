0a8a34a361c847ce33a0fb1a7dda7bd66fd27b8e
Henry Cuffy <hcuffy_5@hotmail.com>
2018-12-01 09:00:09 +0100

    add fields

--- app/components/exam/ExamForm.js
+++ app/components/exam/ExamForm.js
@@ -6,10 +6,55 @@ import styles from '../styles/classroom.css'
 
 const ExamForm = () => (
 	<div>
-		<label className={styles.form_label} htmlFor="titleId">
-			Title:
-		</label>
-		<input name="Title" className="form-control" id="titleId" type="text" />
+		<form className="form-inline">
+			<div>
+				<label className={styles.form_label} htmlFor="titleId">
+					Title:
+				</label>
+				<input name="Title" className="form-control" id="titleId" type="text" />
+			</div>
+			<div>
+				<label className={styles.form_label} htmlFor="cSelect">
+					Select Classroom:
+				</label>
+				<select className="form-control" id="cSelect" type="text">
+					<option className="form-control dropdown">Class1</option>
+					<option className="form-control dropdown">Class2</option>
+				</select>
+			</div>
+			<div>
+				<label className={styles.form_label} htmlFor="cSelect">
+					Select Subject:
+				</label>
+				<select className="form-control" id="sSelect" type="text">
+					<option className="form-control dropdown">Subject1</option>
+					<option className="form-control dropdown">Subject2</option>
+				</select>
+			</div>
+			<div className="form-group row">
+				<label htmlFor="date-input" className="col-2 col-form-label">
+					Date:
+				</label>
+				<div className="col-10">
+					<input className="form-control" type="date" id="date-input" />
+				</div>
+			</div>
+			<div className="form-group row">
+				<label htmlFor="number-input" className={styles.form_label}>
+					Number:
+				</label>
+
+				<input
+					className="form-control"
+					defaultValue="1"
+					type="number"
+					id="number-input"
+					min="1"
+					max="4"
+					step="0.5"
+				/>
+			</div>
+		</form>
 	</div>
 )
 
