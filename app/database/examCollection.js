e445a443bb9890becca5a7a3daa2870bdfcf8d7d
Henry Cuffy <hcuffy_5@hotmail.com>
2018-12-02 08:46:49 +0100

    add new exam function

--- app/database/examCollection.js
+++ app/database/examCollection.js
@@ -1,17 +1,36 @@
 // @flow
+import { saveSuccessful, saveError, entryAlreadyExists } from '../notifications/general'
 
-// const _ = require('lodash')
-// const Datastore = require('nedb')
-// const electron = require('electron')
-// const path = require('path')
+const Datastore = require('nedb')
+const electron = require('electron')
+const path = require('path')
 
-// const userDataPath = (electron.app || electron.remote.app).getPath('userData')
-// const examCollection = new Datastore({
-// 	filename: path.join(userDataPath, 'classroom.db'),
-// 	autoload: true,
-// 	corruptAlertThreshold: 1,
-// 	timestampData: true
-// })
+const userDataPath = (electron.app || electron.remote.app).getPath('userData')
+const examCollection = new Datastore({
+	filename: path.join(userDataPath, 'classroom.db'),
+	autoload: true,
+	corruptAlertThreshold: 1,
+	timestampData: true
+})
 
-
-
+export const addExamData = data => {
+	examCollection.find({ Name: data.Title }, (err, entry) => {
+		if (err) {
+			saveError()
+			return err
+		}
+		if (entry.length > 0) {
+			entryAlreadyExists()
+			return
+		}
+		const newData = data
+		examCollection.insert(newData, (error, doc) => {
+			if (error) {
+				saveError()
+				return error
+			}
+			saveSuccessful()
+			return doc
+		})
+	})
+}
