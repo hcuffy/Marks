63669586ffc0d75e135deaffb953ed3c47f98a8e
Henry Cuffy <hcuffy_5@hotmail.com>
2018-12-23 15:38:59 +0100

    Add student action

--- app/actions/studentActions.js
+++ app/actions/studentActions.js
@@ -1,13 +1,20 @@
+import { ADD_NEW_STUDENT } from './actionTypes'
+import { addNewStudentData } from '../database/studentCollection'
+
 export const addNewStudent = event => {
 	event.preventDefault()
 
 	const formData = {
-		First_Name: event.target.First_Name.value,
-		Last_Name: event.target.Last_Name.value,
+		FirstName: event.target.First_Name.value,
+		LastName: event.target.Last_Name.value,
 		Gender: event.target.Gender.value,
 		Classroom: event.target.Classroom.value
 	}
-
+	addNewStudentData(formData)
 	event.target.reset()
-	console.log(formData)
+
+	return {
+		type: ADD_NEW_STUDENT,
+		payload: {}
+	}
 }
