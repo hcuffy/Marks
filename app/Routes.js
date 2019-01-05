088a7286d72e89db9e366fc4b0327d3b9fc55d83
Henry Cuffy <hcuffy_5@hotmail.com>
2019-01-05 13:52:21 +0100

    Add grades route

--- app/Routes.js
+++ app/Routes.js
@@ -1,12 +1,13 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react'
 import { Switch, Route } from 'react-router'
-import { HOME, SCHOOL, CLASSROOM, STUDENTS } from './constants/routes.json'
+import { HOME, SCHOOL, CLASSROOM, STUDENTS, GRADES } from './constants/routes.json'
 import App from './containers/App'
 import Home from './containers/Home'
 import Classroom from './containers/Classroom'
 import School from './containers/School'
 import Students from './containers/Students'
+import Grades from './containers/Grades'
 
 export default () => (
 	<App>
@@ -15,7 +16,8 @@ export default () => (
 				<Route exact key="1" path={SCHOOL} component={School} />,
 				<Route exact key="1" path={HOME} component={Home} />,
 				<Route exact key="1" path={CLASSROOM} component={Classroom} />,
-				<Route exact key="1" path={STUDENTS} component={Students} />
+				<Route exact key="1" path={STUDENTS} component={Students} />,
+				<Route exact key="1" path={GRADES} component={Grades} />
 			]}
 		</Switch>
 	</App>
