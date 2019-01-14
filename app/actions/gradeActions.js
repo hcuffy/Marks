e0fff9cfbc363d1f8da79f0d7bbbbaffe42ae676
Henry Cuffy <hcuffy_5@hotmail.com>
2019-01-14 18:43:06 +0100

    lift action function

--- app/actions/gradeActions.js
+++ app/actions/gradeActions.js
@@ -8,25 +8,26 @@ import { getAllGrades } from '../database/gradeCollection'
 
 const _ = require('lodash')
 
+export const openGradeClassList = event => {
+	const classroom = event.target.innerText
+	return {
+		type: OPEN_CLASS_LIST,
+		payload: classroom
+	}
+}
+
 export const displayGradeData = event => async dispatch => {
 	const subjectData = {
 		subjectId: event.target.id,
 		subjectName: event.target.innerText
 	}
+
 	const exams = _.filter(await getExamData(), ['SubjectId', subjectData.subjectId])
 	if (exams.length !== 0) {
 		dispatch({
 			type: DISPLAY_EXAM_TABLE,
 			payload: { exams, subjectData }
 		})
-	}
-}
-
-export const openGradeClassList = event => {
-	const classroom = event.target.innerText
-	return {
-		type: OPEN_CLASS_LIST,
-		payload: classroom
 	}
 }
 
