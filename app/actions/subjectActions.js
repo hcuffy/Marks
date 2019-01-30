2b96f32cb467623cbab23d733b82159e17f79fb5
Henry Cuffy <hcuffy_5@hotmail.com>
2019-01-30 18:12:51 +0100

    lint

--- app/actions/subjectActions.js
+++ app/actions/subjectActions.js
@@ -1,53 +1,53 @@
 import {
-  UPDATE_CLASS_LIST,
-  GET_SINGLE_SUBJECT,
-  ADD_NEW_SUBJECT,
-  GET_SUBJECT_LIST
-} from '../constants/actionTypes';
-import { addSubjectData, getAllSubjects } from '../database/subjectCollection';
+	UPDATE_CLASS_LIST,
+	GET_SINGLE_SUBJECT,
+	ADD_NEW_SUBJECT,
+	GET_SUBJECT_LIST
+} from '../constants/actionTypes'
+import { addSubjectData, getAllSubjects } from '../database/subjectCollection'
 
 export const openClassList = event => {
-  event.preventDefault();
+	event.preventDefault()
 
-  const subject = event.target.innerText;
-  return {
-    type: UPDATE_CLASS_LIST,
-    payload: subject
-  };
-};
+	const subject = event.target.innerText
+	return {
+		type: UPDATE_CLASS_LIST,
+		payload: subject
+	}
+}
 
 export const addNewSubject = event => {
-  event.preventDefault();
+	event.preventDefault()
 
-  const formData = {
-    Name: event.target.Name.value,
-    Abbreviation: event.target.Abbreviation.value,
-    Room: event.target.Room.value
-  };
+	const formData = {
+		Name: event.target.Name.value,
+		Abbreviation: event.target.Abbreviation.value,
+		Room: event.target.Room.value
+	}
 
-  event.target.reset();
-  addSubjectData(formData);
-  return {
-    type: ADD_NEW_SUBJECT,
-    payload: {}
-  };
-};
+	event.target.reset()
+	addSubjectData(formData)
+	return {
+		type: ADD_NEW_SUBJECT,
+		payload: {}
+	}
+}
 
 export const getSubjectData = () => async dispatch => {
-  const data = await getAllSubjects();
-  if (data.length !== 0) {
-    dispatch({
-      type: GET_SUBJECT_LIST,
-      payload: { data }
-    });
-  }
-};
+	const data = await getAllSubjects()
+	if (data.length !== 0) {
+		dispatch({
+			type: GET_SUBJECT_LIST,
+			payload: { data }
+		})
+	}
+}
 
 export const showSubject = event => {
-  const subject = event.target.innerText;
-  getSubjectData();
-  return {
-    type: GET_SINGLE_SUBJECT,
-    payload: subject
-  };
-};
+	const subject = event.target.innerText
+	getSubjectData()
+	return {
+		type: GET_SINGLE_SUBJECT,
+		payload: subject
+	}
+}
