c67f0f54c584eb31e5c89994099e63670a125e85
Henry Cuffy <hcuffy_5@hotmail.com>
2019-01-30 18:12:02 +0100

    get all grades

--- app/actions/graphActions.js
+++ app/actions/graphActions.js
@@ -1,4 +1,5 @@
-import { OPEN_GRAPH_CLASS_LIST } from '../constants/actionTypes'
+import { OPEN_GRAPH_CLASS_LIST, GET_ALL_GRADES } from '../constants/actionTypes'
+import { getAllGrades } from '../database/gradeCollection'
 
 export const openGraphClassList = event => {
 	const classroom = event.target.innerText
@@ -13,5 +14,15 @@ export const displayClassGraph = event => {
 	return {
 		type: null,
 		payload: null
+	}
+}
+
+export const getAllGradeData = () => async dispatch => {
+	const grades = await getAllGrades()
+	if (grades.length !== 0) {
+		dispatch({
+			type: GET_ALL_GRADES,
+			payload: { grades }
+		})
 	}
 }
