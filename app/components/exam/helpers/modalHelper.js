42307bf39fa81c537f0bb00eb10d409e5ff95eef
Henry Cuffy <hcuffy_5@hotmail.com>
2019-02-01 17:03:43 +0100

    break up exam modalhelper

--- app/components/exam/helpers/modalHelper.js
+++ app/components/exam/helpers/modalHelper.js
@@ -3,57 +3,69 @@ import styles from '../styles/exam.css'
 
 const _ = require('lodash')
 
-const generateFields = chosenExam => {
+const examTitle = chosenExam => (
+	<div className={styles.form_div_edit}>
+		<label
+			className={styles.form_label_edit}
+			htmlFor={`${_.invert(chosenExam)[chosenExam.Title]}_Id`}
+		>
+			{_.invert(chosenExam)[chosenExam.Title]}:
+		</label>
+		<input
+			name={_.invert(chosenExam)[chosenExam.Title]}
+			className={`${styles.form_input} ${styles.modalInput} form-control`}
+			data-id={`${_.invert(chosenExam)[chosenExam.Title]}_Id`}
+			type="text"
+			defaultValue={chosenExam.Title}
+		/>
+	</div>
+)
+
+const examWieght = chosenExam => (
+	<div className={styles.form_div_edit}>
+		<label
+			className={styles.form_label_edit}
+			htmlFor={`${_.invert(chosenExam)[chosenExam.Weight]}_Id`}
+		>
+			{_.invert(chosenExam)[chosenExam.Weight]}:
+		</label>
+		<input
+			name={_.invert(chosenExam)[chosenExam.Weight]}
+			className={`${styles.form_input} form-control`}
+			data-id={`${_.invert(chosenExam)[chosenExam.Weight]}_Id`}
+			type="number"
+			min="1"
+			max="4"
+			step="0.5"
+			defaultValue={chosenExam.Weight}
+		/>
+	</div>
+)
+
+const examDate = chosenExam => (
+	<div className={styles.form_div_edit}>
+		<label
+			className={styles.form_label_edit}
+			htmlFor={`${_.invert(chosenExam)[chosenExam.Date]}_Id`}
+		>
+			{_.invert(chosenExam)[chosenExam.Date]}:
+		</label>
+		<input
+			name={_.invert(chosenExam)[chosenExam.Date]}
+			className={`${styles.form_input} form-control`}
+			data-id={`${_.invert(chosenExam)[chosenExam.Date]}_Id`}
+			type="date"
+			defaultValue={chosenExam.Date}
+		/>
+	</div>
+)
+
+const generateFields = exam => {
 	const examFields = (
 		<div>
-			<div className={styles.form_div_edit}>
-				<label
-					className={styles.form_label_edit}
-					htmlFor={`${_.invert(chosenExam)[chosenExam.Title]}_Id`}
-				>
-					{_.invert(chosenExam)[chosenExam.Title]}:
-				</label>
-				<input
-					name={_.invert(chosenExam)[chosenExam.Title]}
-					className={`${styles.form_input} ${styles.modalInput} form-control`}
-					data-id={`${_.invert(chosenExam)[chosenExam.Title]}_Id`}
-					type="text"
-					defaultValue={chosenExam.Title}
-				/>
-			</div>
-			<div className={styles.form_div_edit}>
-				<label
-					className={styles.form_label_edit}
-					htmlFor={`${_.invert(chosenExam)[chosenExam.Weight]}_Id`}
-				>
-					{_.invert(chosenExam)[chosenExam.Weight]}:
-				</label>
-				<input
-					name={_.invert(chosenExam)[chosenExam.Weight]}
-					className={`${styles.form_input} form-control`}
-					data-id={`${_.invert(chosenExam)[chosenExam.Weight]}_Id`}
-					type="number"
-					min="1"
-					max="4"
-					step="0.5"
-					defaultValue={chosenExam.Weight}
-				/>
-			</div>
-			<div className={styles.form_div_edit}>
-				<label
-					className={styles.form_label_edit}
-					htmlFor={`${_.invert(chosenExam)[chosenExam.Date]}_Id`}
-				>
-					{_.invert(chosenExam)[chosenExam.Date]}:
-				</label>
-				<input
-					name={_.invert(chosenExam)[chosenExam.Date]}
-					className={`${styles.form_input} form-control`}
-					data-id={`${_.invert(chosenExam)[chosenExam.Date]}_Id`}
-					type="date"
-					defaultValue={chosenExam.Date}
-				/>
-			</div>
+			{examTitle(exam)}
+			{examWieght(exam)}
+			{examDate(exam)}
 		</div>
 	)
 
