18a82e5c24cebaac61642409dc03023052eb9054
Henry Cuffy <hcuffy_5@hotmail.com>
2019-02-06 07:34:44 +0100

    add div for chart

--- app/components/students/StudentsSection.js
+++ app/components/students/StudentsSection.js
@@ -5,9 +5,14 @@ import StudentList from './StudentList'
 
 const StudentsSection = () => (
 	<div>
-		<h2 className={styles.center_header}>Students</h2>
-		<StudentForm />
-		<StudentList />
+		<div>
+			<h2 className={styles.center_header}>Students</h2>
+			<StudentForm />
+			<StudentList />
+		</div>
+		<div className={styles.chart_div}>
+			<h4 className={styles.chart_header}>Charts</h4>
+		</div>
 	</div>
 )
 
