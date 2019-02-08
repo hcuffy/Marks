d54817c2295a45b97d1c40e6b6a152f88fc0b2a6
Henry Cuffy <hcuffy_5@hotmail.com>
2019-02-08 18:12:13 +0100

    remove unecessary console log

--- app/components/students/helpers/chart/chartData.js
+++ app/components/students/helpers/chart/chartData.js
@@ -17,22 +17,17 @@ const filteredGrades = (studentGraphId, grades) => {
 const chartHeader = studentGraphName =>
 	_.isUndefined(studentGraphName) ? 'Student Grades' : studentGraphName
 
-export const chartData = ({ studentGraphName, studentGraphId }, grades) => {
-	const test = filteredGrades(studentGraphId, grades)
-	console.log(test)
-
-	return {
-		datasets: [
-			{
-				label: chartHeader(studentGraphName),
-				fill: false,
-				pointHoverRadius: 20,
-				pointRadius: 5,
-				borderColor: 'rgba(255, 99, 132, 0.6)',
-				backgroundColor: 'rgba(255, 99, 132, 0.6)',
-				pointBackgroundColor: 'rgba(255, 99, 132, 0.6)',
-				data: filteredGrades(studentGraphId, grades)
-			}
-		]
-	}
-}
+export const chartData = ({ studentGraphName, studentGraphId }, grades) => ({
+	datasets: [
+		{
+			label: chartHeader(studentGraphName),
+			fill: false,
+			pointHoverRadius: 20,
+			pointRadius: 5,
+			borderColor: 'rgba(255, 99, 132, 0.6)',
+			backgroundColor: 'rgba(255, 99, 132, 0.6)',
+			pointBackgroundColor: 'rgba(255, 99, 132, 0.6)',
+			data: filteredGrades(studentGraphId, grades)
+		}
+	]
+})
