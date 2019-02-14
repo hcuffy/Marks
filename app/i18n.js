a724fb6f965c1cd13c52f4aaa851e39d1be2207c
Henry Cuffy <hcuffy_5@hotmail.com>
2019-02-14 21:11:00 +0100

    implement auto language detection

--- app/i18n.js
+++ app/i18n.js
@@ -1,9 +1,11 @@
 import i18n from 'i18next'
 import Backend from 'i18next-xhr-backend'
+import LanguageDetector from 'i18next-electron-language-detector'
 import { reactI18nextModule } from 'react-i18next'
 
 i18n
 	.use(Backend)
+	.use(LanguageDetector)
 	.use(reactI18nextModule)
 	.init({
 		backend: {
