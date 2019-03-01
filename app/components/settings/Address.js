b1a6ebfa8effef80f89fa9697ec692aa08c7c45d
Henry Cuffy <hcuffy_5@hotmail.com>
2019-03-01 20:31:10 +0100

    add address form header

--- app/components/settings/Address.js
+++ app/components/settings/Address.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { t } from '../../utils/translationUtil'
 import { addressForm, addressFields } from './helpers/formHelpers'
 import { actionCreators } from '../../actions/index'
 import styles from './styles/settings.css'
@@ -8,7 +9,12 @@ import styles from './styles/settings.css'
 const Address = ({ schoolData, actions }) => {
 	const entry = addressFields(schoolData)
 
-	return <div className={styles.address_wrapper}>{addressForm(entry, actions)}</div>
+	return (
+		<div className={styles.address_wrapper}>
+			<h4 className={styles.center_header}>{t('settings.addressTitle')}</h4>
+			{addressForm(entry, actions)}
+		</div>
+	)
 }
 const mapStateToProps = state => ({ schoolData: state.schoolData })
 
