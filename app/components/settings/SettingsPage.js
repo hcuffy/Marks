8acef4fe3c3367351f1148a4cd09b81dd6ffe7bd
Henry Cuffy <hcuffy_5@hotmail.com>
2019-03-01 07:41:33 +0100

    move settings page

---
+++ app/components/settings/SettingsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './styles/settings.css'
+import Address from './Address'
+
+const Settings = () => (
+	<div className={styles.div_wrapper}>
+		<Address />
+	</div>
+)
+
+export default Settings
