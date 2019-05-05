f9ece7410f9da3221b56a8524e5bc56b7dd1a755
Henry Cuffy <hcuffy_5@hotmail.com>
2019-05-05 17:41:58 +0200

    clean up test

--- test/actions/classroom.spec.js
+++ test/actions/classroom.spec.js
@@ -1,6 +1,5 @@
 import { spy } from 'sinon'
-import * as React from 'react'
-import { changeClassroomTab } from '../../app/actions/classroomActions'
+import * as actions from '../../app/actions/classroomActions'
 import * as types from '../../app/constants/ActionTypes'
 
 jest.mock('../../app/actions/classroomActions')
@@ -14,11 +13,12 @@ describe('classroom actions', () => {
 			type: types.CHANGE_CLASSROOM_TAB,
 			payload: { classTab: '', examTab: 'active' }
 		}
-		const fn = changeClassroomTab(event)
-		expect(fn).toBeInstanceOf(Function)
+
+		const fn = actions.changeClassroomTab(event)
 		const dispatch = spy()
 		fn(dispatch)
 
+		expect(fn).toBeInstanceOf(Function)
 		expect(dispatch.args[0][0]).toEqual(expectedAction)
 	})
 })
