55a611702e50af70ab289d00a1a39ce2fb0fcfe0
Henry Cuffy <hcuffy_5@hotmail.com>
2019-05-06 18:04:34 +0200

    add new test

--- app/actions/__mocks__/classroomActions.js
+++ app/actions/__mocks__/classroomActions.js
@@ -1,4 +1,4 @@
-import { CHANGE_CLASSROOM_TAB } from '../../constants/actionTypes'
+import { CHANGE_CLASSROOM_TAB, ADD_CLASSROOM_DATA } from '../../constants/actionTypes'
 
 const _ = require('lodash')
 
@@ -13,5 +13,19 @@ export const changeClassroomTab = event => dispatch => {
 	dispatch({
 		type: CHANGE_CLASSROOM_TAB,
 		payload: tabUpdate
+	})
+}
+
+export const handleClassData = event => async dispatch => {
+	const formData = {
+		name: event.target.name,
+		teacher: event.target.teacher,
+		code: event.target.code,
+		substitute: event.target.substitute
+	}
+
+	dispatch({
+		type: ADD_CLASSROOM_DATA,
+		payload: { inputData: [formData] }
 	})
 }
--- test/actions/classroom.spec.js
+++ test/actions/classroom.spec.js
@@ -1,6 +1,6 @@
 import { spy } from 'sinon'
 import * as actions from '../../app/actions/classroomActions'
-import * as types from '../../app/constants/ActionTypes'
+import * as types from '../../app/constants/actionTypes'
 
 jest.mock('../../app/actions/classroomActions')
 
@@ -15,6 +15,37 @@ describe('classroom actions', () => {
 		}
 
 		const fn = actions.changeClassroomTab(event)
+		const dispatch = spy()
+		fn(dispatch)
+
+		expect(fn).toBeInstanceOf(Function)
+		expect(dispatch.args[0][0]).toEqual(expectedAction)
+	})
+
+	it('should add new classrooms', () => {
+		const event = {
+			target: {
+				name: 'Biology',
+				teacher: 'Sara Tester',
+				code: 'BIO101',
+				substitute: 'John Tester'
+			}
+		}
+		const expectedAction = {
+			type: types.ADD_CLASSROOM_DATA,
+			payload: {
+				inputData: [
+					{
+						name: 'Biology',
+						teacher: 'Sara Tester',
+						code: 'BIO101',
+						substitute: 'John Tester'
+					}
+				]
+			}
+		}
+
+		const fn = actions.handleClassData(event)
 		const dispatch = spy()
 		fn(dispatch)
 
