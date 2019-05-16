a95bbc72f105928094ccd9e4b271587158f35fbc
Henry Cuffy <hcuffy_5@hotmail.com>
2019-05-16 19:30:28 +0200

    add sidemnu action test

---
+++ test/actions/mock_modules/eventMocks/sidemenuActionEvents.js
@@ -0,0 +1,8 @@
+export const changeButtonColor = {
+	target: {
+		'data-id': 'classroom',
+		getAttribute(value) {
+			return this[value]
+		}
+	}
+}
---
+++ test/actions/sidemenuActions.spec.js
@@ -0,0 +1,31 @@
+/* eslint-disable promise/always-return */
+/* eslint-disable promise/catch-or-return */
+import { spy } from 'sinon'
+import * as actions from '../../app/actions/sidemenuActions'
+import * as types from '../../app/constants/actionTypes'
+import * as events from './mock_modules/eventMocks/sidemenuActionEvents'
+
+jest.mock('../../app/actions/sidemenuActions')
+
+describe('exams actions', () => {
+	it('should change menu background color', () => {
+		const expectedAction = {
+			type: types.HANDLE_MENU_CHANGE,
+			payload: {
+				styleUpdate: {
+					home: '',
+					classroom: '#1dbb90',
+					students: '',
+					exams: '',
+					graphs: '',
+					settings: ''
+				}
+			}
+		}
+		const fn = actions.updateButtonStyle(events.changeButtonColor)
+		const dispatch = spy()
+		expect(fn).toBeInstanceOf(Function)
+		fn(dispatch)
+		expect(dispatch.args[0][0]).toEqual(expectedAction)
+	})
+})
