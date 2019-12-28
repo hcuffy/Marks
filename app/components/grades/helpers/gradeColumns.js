d01ee3e889ed09a187fdcd2dcf806478ffa9d992
Henry Cuffy <hcuffy_5@hotmail.com>
2019-12-28 16:14:34 +0100

    fixed linting issue

--- app/components/grades/helpers/gradeColumns.js
+++ app/components/grades/helpers/gradeColumns.js
@@ -3,38 +3,53 @@ import css from '../styles/grades.css'
 
 const _ = require('lodash')
 
-const customCell = (props, actions) => (
-	<input
-		defaultValue={props.value.score}
-		data-studentid={props.value.studentId}
-		data-subjectname={props.value.subjectName}
-		data-subjectid={props.value.subjectId}
-		data-examid={props.value.examId}
-		data-date={props.value.date}
-		data-weight={props.value.weight}
-		data-id={props.value.gradeId}
-		type="number"
-		onBlur={actions.updateGrade}
-	/>
-)
+const customCell = (props, actions) => {
+	const {
+		score,
+		studentId,
+		subjectName,
+		subjectId,
+		examId,
+		date,
+		weight,
+		gradeId
+	} = props.value
+
+	return (
+		<input
+			defaultValue={score}
+			data-studentid={studentId}
+			data-subjectname={subjectName}
+			data-subjectid={subjectId}
+			data-examid={examId}
+			data-date={date}
+			data-weight={weight}
+			data-id={gradeId}
+			type="number"
+			onBlur={actions.updateGrade}
+		/>
+	)
+}
 
 const badgeColor = weight => {
 	switch (true) {
-	case weight <= 1: {
-		return 'badge-success'
-	}
-	case weight > 2: {
-		return 'badge-primary'
-	}
-	default: {
-		return 'badge-warning'
-	}
+		case weight <= 1: {
+			return 'badge-success'
+		}
+		case weight > 2: {
+			return 'badge-primary'
+		}
+		default: {
+			return 'badge-warning'
+		}
 	}
 }
 
 const customHeader = ({ date, weight }) => (
 	<div>
-		<span className={`badge badge-pill ${badgeColor(weight)} ${css.badge_weight}`}>
+		<span
+			className={`badge badge-pill ${badgeColor(weight)} ${css.badge_weight}`}
+		>
 			<i className="fas fa-weight-hanging" /> {weight}
 		</span>
 		<span className={`badge badge-light badge-pill ${css.badge_date}`}>
@@ -53,13 +68,11 @@ const averageColumn = () => ({
 const customFooter = ({ data }, iterator) => {
 	const gradesArray = []
 	for (let i = 0; i < data.length; i += 1) {
-		// eslint-disable-next-line no-underscore-dangle
 		const { score } = data[i]._original.grades[iterator]
 
-		if(parseInt(score, 10) !== 0){
+		if (parseInt(score, 10) !== 0) {
 			gradesArray.push(parseInt(score, 10))
 		}
-
 	}
 	console.log(gradesArray)
 	const examAverage = _.round(_.mean(gradesArray), 2)
@@ -102,14 +115,16 @@ export const gradeColumns = ({ t, newData, actions }) => {
 					accessor: 'gender',
 					width: 40,
 					style: { textAlign: 'center' }
-				}, {
+				},
+				{
 					Header: t('grades.fullName'),
 					accessor: 'name',
 					width: 150,
 					headerStyle: { textAlign: 'left' }
 				}
 			]
-		}, {
+		},
+		{
 			Header: t('grades.examHeader'),
 			columns: customColumn(newData, actions)
 		}
