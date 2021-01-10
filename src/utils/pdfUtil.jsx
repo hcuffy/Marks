ba3e8e406f191af2d7a61e2542d694c41a014151
Henry Cuffy <henry.cuffy@fleetster.net>
2021-01-10 10:02:47 +0100

    - fix pdf auto trigger

--- src/utils/pdfUtil.jsx
+++ src/utils/pdfUtil.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import _ from 'lodash';
+import {jsPDF} from 'jspdf';
 import {Button} from 'reactstrap';
+
+const pdfDocument = new jsPDF('landscape');
 
 function downloadPDF(itemToPDF, nameOfChart, saveAs) {
     // eslint-disable-next-line no-undef
@@ -9,9 +12,8 @@ function downloadPDF(itemToPDF, nameOfChart, saveAs) {
     if (_.isNull(canvas)) {
         return;
     }
-    const JsPDF = require('jspdf');
+
     const canvasImg = canvas.toDataURL('image/png', 1.0);
-    const pdfDocument = new JsPDF('landscape');
     pdfDocument.setFontSize(15);
     pdfDocument.text(15, 15, nameOfChart);
     pdfDocument.addImage(canvasImg, 'png', 10, 10, 280, 150);
@@ -20,7 +22,7 @@ function downloadPDF(itemToPDF, nameOfChart, saveAs) {
 
 export function PDFbutton(styling, saveText, chartTitle) {
     return (
-        <Button className={styling} onClick={downloadPDF('canvas', chartTitle, 'chart')}>
+        <Button className={styling} onClick={() => downloadPDF('canvas', chartTitle, 'chart')}>
             <i className='fas fa-file-pdf fa-2x' /> <br />
             {saveText}
         </Button>
