d1ee1211ae0fd892652b49331035f2b059567215
Henry Cuffy <henry.cuffy@fleetster.net>
2021-03-07 13:00:27 +0100

    - fix calendar language

--- src/components/calendar/CalendarComponent.jsx
+++ src/components/calendar/CalendarComponent.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 import {withTranslation} from 'react-i18next';
 
 import CalendarElement from './Calendar';
 import InputDialog from './CalendarInputDialog';
+import {getUserLocale} from '../../utils';
 import css from './style.css';
 
 function CalendarComponent({t}) {
+    moment.locale(getUserLocale());
+
     return (
         <div className={css.section_wrapper}>
             <h4 className={css.main_header}>{t('calendar.sectionTitle')}</h4>
